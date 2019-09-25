cfbc8c7ecea3c66dfee5cb31416fe525857c033e
Sunil Vurity <suvuri@microsoft.com>
2019-09-25 10:07:55 -0700

    changes

---
+++ DirectLineClient/Program.cs
@@ -0,0 +1,198 @@
+﻿namespace DirectLineSampleClient
+{
+    using System;
+    using System.Configuration;
+    using System.Diagnostics;
+    using System.Linq;
+    using System.Threading.Tasks;
+    using Microsoft.Bot.Connector.DirectLine;
+    using Newtonsoft.Json;
+    using WebSocketSharp;
+
+
+    public class Program
+    {
+        private static string directLineSecret = ConfigurationManager.AppSettings["DirectLineSecret"];
+        private static string botId = ConfigurationManager.AppSettings["BotId"];
+        private static string fromUser = "DirectLineSampleClientUser";
+
+        public static void Main(string[] args)
+        {
+            StartBotConversation().Wait();
+        }
+
+        private static async Task StartBotConversation()
+        {
+            DirectLineClient client = new DirectLineClient(directLineSecret);
+
+            var conversation = await client.Conversations.StartConversationAsync();
+            await StartPollingAsync(client, conversation.ConversationId);
+            //await OpenWebsocketListner(client, conversation.StreamUrl, conversation.ConversationId);
+        }
+
+        private static async Task OpenWebsocketListner(DirectLineClient client, string streamUrl, string conversationId)
+        {
+            using (var webSocketClient = new WebSocket(streamUrl))
+            {
+                webSocketClient.OnMessage += WebSocketClient_OnMessage;
+                // You have to specify TLS version to 1.2 or connection will be failed in handshake.
+                webSocketClient.SslConfiguration.EnabledSslProtocols = System.Security.Authentication.SslProtocols.Tls12;
+                webSocketClient.Connect();
+
+                while (true)
+                {
+                    string input = Console.ReadLine().Trim();
+
+                    if (input.ToLower() == "exit")
+                    {
+                        break;
+                    }
+                    else
+                    {
+                        if (input.Length > 0)
+                        {
+                            Activity userMessage = new Activity
+                            {
+                                From = new ChannelAccount(fromUser),
+                                Text = input,
+                                Type = ActivityTypes.Message
+                            };
+
+                            await client.Conversations.PostActivityAsync(conversationId, userMessage);
+                        }
+                    }
+                }
+            }
+        }
+        private static async Task StartPollingAsync(DirectLineClient client, string conversationId)
+        {
+            new System.Threading.Thread(async () => await ReadBotMessagesAsync(client, conversationId)).Start();
+
+            Console.Write("Command> ");
+
+            while (true)
+            {
+                string input = Console.ReadLine().Trim();
+
+                if (input.ToLower() == "exit")
+                {
+                    break;
+                }
+                else
+                {
+                    if (input.Length > 0)
+                    {
+                        Activity userMessage = new Activity
+                        {
+                            From = new ChannelAccount(fromUser),
+                            Text = input,
+                            Type = ActivityTypes.Message
+                        };
+
+                        await client.Conversations.PostActivityAsync(conversationId, userMessage);
+                    }
+                }
+            }
+        }
+
+        private static void WebSocketClient_OnMessage(object sender, MessageEventArgs e)
+        {
+            // Occasionally, the Direct Line service sends an empty message as a liveness ping. Ignore these messages.
+            if (string.IsNullOrWhiteSpace(e.Data))
+            {
+                return;
+            }
+
+            var activitySet = JsonConvert.DeserializeObject<ActivitySet>(e.Data);
+            var activities = from x in activitySet.Activities
+                             where x.From.Id == botId
+                             select x;
+
+            foreach (Activity activity in activities)
+            {
+                Console.WriteLine(activity.Text);
+
+                if (activity.Attachments != null)
+                {
+                    foreach (Attachment attachment in activity.Attachments)
+                    {
+                        switch (attachment.ContentType)
+                        {
+                            case "application/vnd.microsoft.card.hero":
+                                RenderHeroCard(attachment);
+                                break;
+
+                            case "image/png":
+                                Console.WriteLine($"Opening the requested image '{attachment.ContentUrl}'");
+
+                                Process.Start(attachment.ContentUrl);
+                                break;
+                        }
+                    }
+                }
+
+                Console.Write("Command> ");
+            }
+        }
+
+        private static async Task ReadBotMessagesAsync(DirectLineClient client, string conversationId)
+        {
+            string watermark = null;
+
+            while (true)
+            {
+                var activitySet = await client.Conversations.GetActivitiesAsync(conversationId, watermark);
+                watermark = activitySet?.Watermark;
+
+                var activities = from x in activitySet.Activities
+                                 where x.From.Id == botId
+                                 select x;
+
+                foreach (Activity activity in activities)
+                {
+                    Console.WriteLine(activity.Text);
+
+                    if (activity.Attachments != null)
+                    {
+                        foreach (Attachment attachment in activity.Attachments)
+                        {
+                            switch (attachment.ContentType)
+                            {
+                                case "application/vnd.microsoft.card.hero":
+                                    RenderHeroCard(attachment);
+                                    break;
+
+                                case "image/png":
+                                    Console.WriteLine($"Opening the requested image '{attachment.ContentUrl}'");
+
+                                    Process.Start(attachment.ContentUrl);
+                                    break;
+                            }
+                        }
+                    }
+
+                    Console.Write("Command> ");
+                }
+
+                await Task.Delay(TimeSpan.FromSeconds(1)).ConfigureAwait(false);
+            }
+        }
+
+        private static void RenderHeroCard(Attachment attachment)
+        {
+            const int Width = 70;
+            Func<string, string> contentLine = (content) => string.Format($"{{0, -{Width}}}", string.Format("{0," + ((Width + content.Length) / 2).ToString() + "}", content));
+
+            var heroCard = JsonConvert.DeserializeObject<HeroCard>(attachment.Content.ToString());
+
+            if (heroCard != null)
+            {
+                Console.WriteLine("/{0}", new string('*', Width + 1));
+                Console.WriteLine("*{0}*", contentLine(heroCard.Title));
+                Console.WriteLine("*{0}*", new string(' ', Width));
+                Console.WriteLine("*{0}*", contentLine(heroCard.Text));
+                Console.WriteLine("{0}/", new string('*', Width + 1));
+            }
+        }
+    }
+}
